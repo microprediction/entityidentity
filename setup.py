fd080a49ab3deacd7c26205b537e4fca81b9bc56
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2025-09-30 20:17:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="entityidentity",
-    version="0.0.1",
+    version="0.0.2",
     author="Peter Cotton",
     author_email="",
     description="Ontology / Entity Resolution",
